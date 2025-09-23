85a2f1c3490c361b7f6a1099cb76d04b9309b4d9
Mardon <github@mrdn.slmail.me>
2025-09-23 11:28:18 +0500

    feat: added background command for highlighting text and save to API

---
+++ apps/extension/src/utils/types.ts
@@ -0,0 +1,11 @@
+import type { route } from '@finest/utils/types'
+import { hc } from 'hono/client'
+
+const apiUrl = import.meta.env.VITE_API_URL
+if (!apiUrl) {
+  throw new Error("VITE_API_URL is not defined");
+}
+
+const client = hc<route>(apiUrl)
+
+export { client };
--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -3,5 +3,18 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-solid'],
-  srcDir: 'src'
+  srcDir: 'src',
+  manifest: {
+    commands: {
+      "highlight": {
+        "suggested_key": {
+          "default": "Ctrl+Shift+Y",
+          "mac": "Command+Shift+Y"
+        },
+        "description": "Highlight selected text"
+      }
+    },
+    permissions: ['scripting', 'tabs'],
+    host_permissions: ["<all_urls>"],
+  }
 });
