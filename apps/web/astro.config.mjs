b5d3502fcbf9d0c1f4049737cd4c47444a666bbe
Mardon <github@mrdn.slmail.me>
2025-10-07 14:28:26 +0500

    feat: added Solid.js

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.CF_WEB_URL,
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [solidJs()]
 });
